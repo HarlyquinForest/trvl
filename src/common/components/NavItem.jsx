4d74636e3c6c57d5d3a3c5165abbf28607ae9fd9
HarlyquinForest <aminyousefnejad28@gmail.com>
2022-09-23 14:28:04 +0330

    useRouter hook using for getting current url and indicate related NavItem component in navbar

--- src/common/components/NavItem.jsx
+++ src/common/components/NavItem.jsx
@@ -1,12 +1,16 @@
 import NextLink from "next/link";
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 
 const NavItem = ({ url, title }) => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <NextLink href={url}>
-      <a className="text-lg p-2 font-light hover:underline underline-offset-8 decoration-4 hover:scale-105 transition-all "
-         style={{textDecoration: router.pathname == url ? 'underline' : 'none'}}>
+      <a
+        className="text-lg p-2 font-light hover:underline underline-offset-8 decoration-4 hover:scale-105 transition-all "
+        style={{
+          textDecoration: router.pathname == url ? "underline" : "none",
+        }}
+      >
         {title}
       </a>
     </NextLink>
